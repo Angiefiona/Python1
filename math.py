eeceed5e79b2d55697379a2aa1b842ebec4a01c9
Angiefiona <angellamumoh@gmail.com>
2023-04-14 16:10:34 +0300

    first commit

---
+++ math.py
@@ -0,0 +1,23 @@
+def floordivision(a,b):
+    answer = a//b
+    return answer
+
+def modulus(x,y):
+    answer = x%y
+    return answer
+
+def addition(u,v):
+    result = u+v
+    return result
+
+def subtraction(m,n):
+    answer = m-n
+    return answer
+
+def multiply(s,t):
+    answer = s*t
+    return answer
+
+def division(j,k):
+    answer = j/k
+    return answer
